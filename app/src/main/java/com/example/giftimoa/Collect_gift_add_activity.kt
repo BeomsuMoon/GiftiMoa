5edd9cb4f2e88ebe77d1419a6385eccc80f37418
1817-3 <kho4514@naver.com>
2023-11-03 18:12:27 +0900

    Collect 기능 갤러리에서 사진 가져오기 추가

--- app/src/main/java/com/example/giftimoa/Collect_gift_add_activity.kt
+++ app/src/main/java/com/example/giftimoa/Collect_gift_add_activity.kt
@@ -1,7 +1,11 @@
 package com.example.giftimoa
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.giftimoa.databinding.LayoutCollectGiftAddBinding
 
 class Collect_gift_add_activity : AppCompatActivity() {
@@ -11,13 +15,36 @@ class Collect_gift_add_activity : AppCompatActivity() {
         binding = LayoutCollectGiftAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //액션바 활성화
         setSupportActionBar(binding.myToolbar)
         supportActionBar?.setDisplayShowTitleEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "기프티콘 등록"
+
+        galleryClickEvent()
+        //갤러리 호출
     }
 
 
+    private fun galleryClickEvent(){
+        binding.uploadImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            activityResult.launch(intent)
+        }
+    }
+
+    private val activityResult:ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+
+        if(it.resultCode == RESULT_OK && it.data != null){
+            val uri = it.data!!.data
+
+            Glide.with(this)
+                .load(uri)
+                .into(binding.uploadImage)
+        }
+    }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
---
+++ app/src/main/java/com/example/giftimoa/dto/Collect_Gift.kt
@@ -0,0 +1,9 @@
+package com.example.giftimoa.dto
+
+data class Collect_Gift(
+    val brand : String,
+    val image: Int,
+    val date: Int,
+    val menu: String,
+    val havedate: Int
+)
