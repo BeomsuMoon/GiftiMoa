12740ec007697bed15885b4796fcfff6fc089ca9
BeomsuMoon <105366422+BeomsuMoon@users.noreply.github.com>
2023-11-18 23:12:49 +0900

    2023/11/18 앱 아이콘 추가 및 인트로 추가

--- app/src/main/java/com/example/giftimoa/Splash_activity.kt
+++ app/src/main/java/com/example/giftimoa/Splash_activity.kt
@@ -11,11 +11,6 @@ class Splash_activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_splash)
 
-
-        // Hide the status bar.
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        actionBar?.hide()
-
         Handler().postDelayed({
             val intent = Intent(this,Login_activity::class.java)
             startActivity(intent)
@@ -24,9 +19,5 @@ class Splash_activity : AppCompatActivity() {
     }
     companion object{
         private const val DULATION : Long = 1500
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
     }
 }
