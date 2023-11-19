e61a5b6be02157cfb1a88e1e804cccf03678bc71
BeomsuMoon <105366422+BeomsuMoon@users.noreply.github.com>
2023-11-19 16:12:07 +0900

    2023/11/19 기프티콘 알림 기능 완료

--- app/src/main/java/com/example/giftimoa/AlarmReceiver.kt
+++ app/src/main/java/com/example/giftimoa/AlarmReceiver.kt
@@ -9,6 +9,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class AlarmReceiver : BroadcastReceiver() {
     var manager: NotificationManager? = null
@@ -34,7 +37,9 @@ class AlarmReceiver : BroadcastReceiver() {
         val pendingIntent = PendingIntent.getActivity(context, 101, intent2, PendingIntent.FLAG_UPDATE_CURRENT)
 
         //알림창 제목
-        builder!!.setContentTitle("$giftName 의 기한이 만료되기 전에 사용해주세요!")
+        val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
+        val currentTime = sdf.format(Date())
+        builder!!.setContentTitle("$giftName 의 기한이 만료되기 전에 사용해주세요! (알림 시간: $currentTime)")
         //알림창 아이콘
         builder!!.setSmallIcon(R.mipmap.ic_launcher)
         //알림창 터치시 자동 삭제
@@ -46,8 +51,8 @@ class AlarmReceiver : BroadcastReceiver() {
 
     companion object {
         //오레오 이상은 반드시 채널을 설정해줘야 Notification이 작동함
-        private const val CHANNEL_ID = "channel1"
-        private const val CHANNEL_NAME = "Channel1"
+        private const val CHANNEL_ID = "GIFTIMOA"
+        private const val CHANNEL_NAME = "GIFTIMOA"
     }
 }
 
--- app/src/main/java/com/example/giftimoa/bottom_nav_fragment/Menu_Fragment.kt
+++ app/src/main/java/com/example/giftimoa/bottom_nav_fragment/Menu_Fragment.kt
@@ -7,12 +7,12 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.giftimoa.AlarmReceiver
@@ -89,14 +89,9 @@ class Menu_Fragment : Fragment() {
             binding.lNotiTime.isClickable = isChecked
 
             if (!isChecked) {
-                binding.lNotiFirst.setOnClickListener(null)
-                binding.lNotiInterval.setOnClickListener(null)
-                binding.lNotiTime.setOnClickListener(null)
-                binding.lNotiAlram.setOnClickListener(null)
+                scheduleAlarms()
             } else {
-                binding.lNotiFirst.setOnClickListener { showNumberPickerFirstDialog() }
-                binding.lNotiInterval.setOnClickListener { showNotificationIntervalPickerDialog() }
-                binding.lNotiTime.setOnClickListener { showTimePickerDialog() }
+                cancelAlarms()
             }
 
             setTextColor(isChecked)
@@ -151,8 +146,6 @@ class Menu_Fragment : Fragment() {
 
     }
 
-
-
     private fun getNicknameFromServer(userEmail: String?) {
         val client = OkHttpClient()
         val url = "http://3.35.110.246:3306/getNicknameByEmail" // 서버의 닉네임 확인 엔드포인트
@@ -182,6 +175,82 @@ class Menu_Fragment : Fragment() {
         }
     }
 
+    //기프티콘 알림 활성화
+    fun scheduleAlarms() {
+        // ViewModel을 통해 기프티콘 리스트를 가져옵니다.
+        val viewModel = ViewModelProvider(requireActivity()).get(Gificon_ViewModel::class.java)
+        val gifticonList = viewModel.collectGifts.value
+
+        // 기프티콘 리스트가 비어있지 않을 때만 알림을 설정합니다.
+        if (!gifticonList.isNullOrEmpty()) {
+            // SharedPreferences에서 알림 설정 정보를 불러옵니다.
+            val sharedPreferences = requireActivity().getSharedPreferences("user_data", Context.MODE_PRIVATE)
+            val firstNoti = sharedPreferences.getInt("first_noti", 0)
+            val intervalNoti = sharedPreferences.getInt("interval_noti", 0)
+            val timeNoti = sharedPreferences.getString("time_noti", "")
+
+            for (gifticon in gifticonList) {
+                // 최초 알림 설정
+                setAlarm(firstNoti, gifticon)
+                // 주기적 알림 설정
+                setRepeatedAlarm(intervalNoti, timeNoti, gifticon)
+            }
+        }
+    }
+    
+    
+    //기프티콘 알림 비활성화
+    fun cancelAlarms() {
+        // ViewModel을 통해 기프티콘 리스트를 가져옵니다.
+        val viewModel = ViewModelProvider(requireActivity()).get(Gificon_ViewModel::class.java)
+        val gifticonList = viewModel.collectGifts.value
+
+        // 기프티콘 리스트가 비어있지 않을 때만 알람을 취소합니다.
+        if (!gifticonList.isNullOrEmpty()) {
+            val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+            for (gifticon in gifticonList) {
+                val intent = Intent(context, AlarmReceiver::class.java)
+                // 각 기프티콘에 대한 고유한 PendingIntent를 생성합니다.
+                val pendingIntent = PendingIntent.getBroadcast(context, gifticon.id, intent, PendingIntent.FLAG_IMMUTABLE or 0)
+                // 알람 취소
+                alarmManager.cancel(pendingIntent)
+            }
+        }
+    }
+
+    //기프티콘 주기적 알림 설정
+    fun setRepeatedAlarm(interval: Int, time: String?, gifticon: Collect_Gift) {
+        val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, AlarmReceiver::class.java).apply {
+            putExtra("giftName", gifticon.giftName)
+        }
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE or 0)
+
+        // String 형식의 effectiveDate를 Date 형식으로 변환합니다.
+        val sdf = SimpleDateFormat("yyyy-MM-dd")
+        val gifticonEffectiveDate: Date = sdf.parse(gifticon.effectiveDate)
+
+        // SharedPreferences에서 알림 시간과 주기를 불러옵니다.
+        val alarmHour = time?.split(":")?.get(0)?.toInt() ?: 0
+        val alarmMinute = time?.split(":")?.get(1)?.toInt() ?: 0
+
+        // 사용자가 설정한 알림을 받고자 하는 날짜와 시간을 계산합니다.
+        val calendar = Calendar.getInstance().apply {
+            val format = SimpleDateFormat("HH:mm")
+            val newTime = format.format(gifticonEffectiveDate)
+            add(Calendar.DAY_OF_YEAR, -interval)
+            set(Calendar.HOUR_OF_DAY, alarmHour)
+            set(Calendar.MINUTE, alarmMinute)
+            set(Calendar.SECOND, 0)
+        }
+        // 알림 주기를 밀리세컨드로 변환
+        val intervalMillis = interval * 24 * 60 * 60 * 1000L
+        // 알람 설정
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, intervalMillis, pendingIntent)
+    }
+
+
     //등록한 기프티콘의 유효기간 만료 안내 알림
     fun setAlarm(daysBefore: Int, gifticon: Collect_Gift) {
         val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
@@ -194,15 +263,39 @@ class Menu_Fragment : Fragment() {
         val sdf = SimpleDateFormat("yyyy-MM-dd")
         val gifticonEffectiveDate: Date = sdf.parse(gifticon.effectiveDate)
 
-        // 사용자가 설정한 알림을 받고자 하는 날짜를 계산합니다.
+        // SharedPreferences에서 알림 시간과 주기를 불러옵니다.
+        val sharedPreferences = requireActivity().getSharedPreferences("user_data", Context.MODE_PRIVATE)
+        val alarmTime = sharedPreferences.getString("time_noti", "09:00") ?: "09:00"
+        val alarmInterval = sharedPreferences.getInt("interval_noti", 1)
+        val alarmHour = alarmTime.split(":")[0].toInt()
+        val alarmMinute = alarmTime.split(":")[1].toInt()
+
+        // 사용자가 설정한 알림을 받고자 하는 날짜와 시간을 계산합니다.
         val calendar = Calendar.getInstance().apply {
             time = gifticonEffectiveDate
             add(Calendar.DAY_OF_YEAR, -daysBefore)
+            set(Calendar.HOUR_OF_DAY, alarmHour)
+            set(Calendar.MINUTE, alarmMinute)
+            set(Calendar.SECOND, 0)
         }
 
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        // 알림 주기를 밀리세컨드로 변환
+        val intervalMillis = alarmInterval * 24 * 60 * 60 * 1000L
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // API 레벨 23 이상에서는 setExactAndAllowWhileIdle() 함수를 사용
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // API 레벨 19 이상에서는 setExact() 함수를 사용
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        } else {
+            // API 레벨 19 미만에서는 set() 함수를 사용
+            alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        }
     }
-    //마감임박 최초 알림
+
+
+    //마감임박 최초 알림 다이얼로그
     fun showNumberPickerFirstDialog() {
         val dialogBinding = DialogNumpickBinding.inflate(LayoutInflater.from(requireContext()))
 
@@ -246,13 +339,13 @@ class Menu_Fragment : Fragment() {
 
     }
 
-
+    //마감 주기 알림 다이얼로그
     fun showNotificationIntervalPickerDialog() {
         val dialogBinding = DialogNumpickBinding.inflate(LayoutInflater.from(requireContext()))
 
         // 넘버픽커 설정
         dialogBinding.npSelect.minValue = 1
-        dialogBinding.npSelect.maxValue = 30
+        dialogBinding.npSelect.maxValue = 10
         dialogBinding.npSelect.wrapSelectorWheel = false
 
         // 타이틀과 단위 텍스트 설정
@@ -279,9 +372,19 @@ class Menu_Fragment : Fragment() {
             // 알람 설정
             alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),
                 intervalMillis, pendingIntent)
+
+            // SharedPreferences에 알림 주기 설정값 저장
+            val sharedPreferences = requireActivity().getSharedPreferences("user_data", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putInt("interval_noti", selectedInterval)
+            editor.apply()
+
+            // 선택된 알림 주기를 로그로 출력
+            Log.d("NotificationInterval", "Selected interval: $selectedInterval")
         }
     }
-
+    
+    //알림 받는 시간 다이얼로그
     fun showTimePickerDialog() {
         val dialogBinding = DialogNumpickBinding.inflate(LayoutInflater.from(requireContext()))
         val times = arrayOf("09:00", "13:00", "18:00")  // 시간 목록
@@ -294,20 +397,28 @@ class Menu_Fragment : Fragment() {
         val builder = AlertDialog.Builder(requireContext()).setView(dialogBinding.root)
         val alertDialog = builder.show()
 
-        // '완료' 버튼 클릭 시 동작
-        dialogBinding.btnComplete.setOnClickListener {
-            alertDialog.dismiss()
-        }
-
         // 아이템 목록 설정
         dialogBinding.npSelect.minValue = 0
         dialogBinding.npSelect.maxValue = times.size - 1
         dialogBinding.npSelect.displayedValues = times
         dialogBinding.npSelect.wrapSelectorWheel = false
-        dialogBinding.npSelect.setOnValueChangedListener { _, _, newVal ->
-            binding.tvNotiSettingTime.text = times[newVal]
+
+        // '완료' 버튼 클릭 시 동작
+        dialogBinding.btnComplete.setOnClickListener {
+            val selectedTime = times[dialogBinding.npSelect.value]
+            binding.tvNotiSettingTime.text = selectedTime
+
+            // SharedPreferences에 알림 시간 설정값 저장
+            val sharedPreferences = requireActivity().getSharedPreferences("user_data", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString("time_noti", selectedTime)
+            editor.apply()
+
+            // 다이얼로그 창 닫기
+            alertDialog.dismiss()
         }
     }
+
 
     fun setTextColor(isChecked: Boolean) {
         val color = if (isChecked) "#000000" else "#939393"
