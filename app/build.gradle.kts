c85ab79330d5c9ddc1009dab1acb3a6a08ae40a3
BeomsuMoon <105366422+BeomsuMoon@users.noreply.github.com>
2023-11-25 12:33:46 +0900

    2023/11/25 Moon 집에서 하다가 학교에서 하기위한 커밋

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
 
+    //웹소켓 라이브러리
+    implementation("io.socket:socket.io-client:2.0.0"){
+        exclude("org.json","json")
+    }
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/giftimoa/adpater_list/RecyclerViewChattingListAdapter.kt
+++ app/src/main/java/com/example/giftimoa/adpater_list/RecyclerViewChattingListAdapter.kt
@@ -1,5 +1,36 @@
 package com.example.giftimoa.adpater_list
 
-class RecyclerViewChattingListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.giftimoa.databinding.ItemChattingCardviewBinding
+import com.example.giftimoa.dto.ChatItem
 
+class RecyclerViewChattingListAdapter : RecyclerView.Adapter<RecyclerViewChattingListAdapter.ViewHolder>() {
+    private val chatRooms = mutableListOf<ChatItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemChattingCardviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val chatRoom = chatRooms[position]
+        holder.bind(chatRoom)
+    }
+
+    override fun getItemCount() = chatRooms.size
+
+    fun submitList(newChatRooms: List<ChatItem>) {
+        chatRooms.clear()
+        chatRooms.addAll(newChatRooms)
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(private val binding: ItemChattingCardviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(ChatItem: ChatItem) {
+            //binding.userNickname.text = ChatItem.nickname
+            binding.userMessage.text = ChatItem.message
+        }
+    }
 }
